33e5378286c40fd2fa1c2c95a1c0a51c29d3024a
tsapuanq <sapuantalaspai@gmail.com>
2025-10-09 23:38:05 +0500

    исправил ошибку с запятой

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -21,6 +21,6 @@ __all__ = [
     'mean_squared_error', 'MSE',
     'mean_absolute_error', 'MAE',
     'root_mean_squared_error', 'RMSE',
-    'mean_absolute_percentage_error', 'MAPE'
+    'mean_absolute_percentage_error', 'MAPE',
     'accuracy', 'recall'
 ]
